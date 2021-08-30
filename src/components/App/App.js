5177ad02d5e4b56496de7d886ccd663eb7af5a61
Gremwiz1 <gremwiz@yandex.ru>
2021-08-30 20:53:11 +0300

    added structure folders and files in project

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Portfolio/Portfolio";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+
+function App() {
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" >
+                    <Main />
+                </Route>
+                <Route exact path="/movies">
+                    <Movies />
+                </Route>
+                <Route exact path="/saved-movies">
+                    <SavedMovies />
+                </Route>
+                <Route exact path="/profile">
+                    <Profile />
+                </Route>
+                <Route exact path="/signin">
+                    <Login />
+                </Route>
+                <Route exact path="/signup">
+                    <Register />
+                </Route>
+            </Switch>
+        </>
+    )
+};
+export default App;
