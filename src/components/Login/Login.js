4a6f0c7c093fe9c4734e7debf163cb8908e9b62e
Gremwiz1 <gremwiz@yandex.ru>
2021-08-31 15:27:04 +0300

    Added component Login in desktop version

---
+++ src/components/Login/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+    return (
+        <section className="login">
+            <Link to="/" className="login__logo"></Link>
+            <h1 className="login__title">Рады видеть!</h1>
+            <form className="login__form">
+                <fieldset className="login__fieldset">
+                    <p className="login__text">E-mail</p>
+                    <input className="login__input" type="email" required />
+                    <span></span>
+                    <p className="login__text">Пароль</p>
+                    <input className="login__input" type="password" required minLength="8" />
+                    <span></span>
+                </fieldset>
+                <div className="login__bottom">
+                    <button className="login__button" type="submit">Войти</button>
+                    <div className="login__links">
+                        <p className="login__answer">Ещё не зарегистрированы?</p>
+                        <Link className="login__link" to="/signup">Регистрация</Link>
+                    </div>
+                </div>
+            </form>
+
+        </section>
+    )
+};
+export default Login;
