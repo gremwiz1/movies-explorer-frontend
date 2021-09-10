733f14c4d0ce5eea75d47dc458b6d77712d4ba3c
Gremwiz1 <gremwiz@yandex.ru>
2021-09-11 00:58:23 +0300

    Added foundError in saved-movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -9,7 +9,7 @@ function MoviesCardList({ moviesCollection, isSaved, isLoadingMovies, savedMovie
     return (
         <section className="movies-card-list">
             {isLoadingMovies ? <Preloader /> : ""}
-            {pathname === "/movies" ? <span className="search-form__error">{foundError ? "Ничего не найдено" : ""}</span> : ""}
+            <span className="search-form__error">{foundError ? "Ничего не найдено" : ""}</span>
             <span className="server__error">{serverError ? "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз" : ""}</span>
             <ul className="movies__collection">
                 {moviesCollection.map((movies) => {
